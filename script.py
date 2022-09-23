a5f94f6bcbb105e3e7c74095f2ec531bbcf39084
Michael Wilt <michaeljwilt@outlook.com>
2022-09-23 17:38:05 -0600

    Add files via upload
    Initial script file for automation project

---
+++ script.py
@@ -0,0 +1,34 @@
+import os
+
+# Initial input for Folder Creation
+fileName = input("Enter file name ")
+path = "/Users/michaelwilt/Desktop/python course/" + fileName
+
+
+# Creation of new Directory
+try:
+    os.mkdir(path)
+except OSError:
+    print("Creation of the directory %s failed" % path)
+else:
+    print("Successfully created the directory %s " % path)
+
+
+# Call Directory and Open in VS Code
+os.chdir(path)
+# os.chdir("/Users/michaelwilt/Desktop/python_course" + fileName)
+cwd = os.getcwd()
+os.system("code .")
+
+
+# ATTEMPTS TO TRY BASIC GIT COMMANDS
+# try:
+#     os.system("git init")
+#     os.system("git add .")
+#     os.system("git commit -m 'Initial Commit'")
+
+# except OSError:
+#     print("Creation of the git repository %s failed")
+# else:
+#     print("Successfully created the git repository %s ")
+
